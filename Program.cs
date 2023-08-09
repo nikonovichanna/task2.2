f0f1724d39de8bcb91dd25d9bf9200305e13d2e5
nikonovichanna <nikonovicvhhanna@gmail.com>
2023-08-09 21:25:52 +0300

    add task2.2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int number = new Random().Next (1, 100);
+System.Console.WriteLine(number);
+if (number < 100)
+{
+   System.Console.WriteLine("третьей цифры нет"); 
+}
+else
+{ 
+    while (number > 999)
+    {
+    number = number / 10;
+    }
+    number = number % 10;
+    System.Console.WriteLine(number);
+}
